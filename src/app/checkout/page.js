5862bf11c47e2ade9623174501b04a45082b472b
Pratik Soni <pratiksoni1402@gmail.com>
2024-01-18 12:09:20 +0530

    created checkout page

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -2,16 +2,19 @@ import Shippingdetail from "./shipping-details"
 import Shoppingbag from "./shopping-bag"
 export default function Checkout() {
     return (
-        <div className="checkout-page">
+        <div className="checkout-page bg-[#faf2ec] pb-10">
             <div className="container">
+                <div className="heading text-center pt-5 pb-10 font-crimson text-[#3c2f27] text-4xl">
+                    Secure Checkout
+                </div>
                 <div className="grid grid-cols-12 gap-5">
-                    <div className="xl:col-span-7 lg:col-span-7 md:col-span-5 sm:col-span-12 col-span-12">
+                    <div className="xl:col-span-7 lg:col-span-7 md:col-span-5 lg:order-1 order-2 sm:col-span-12 col-span-12">
                         <div className="left-section">
                             <Shippingdetail />
                         </div>
                     </div>
-                    <div className="xl:col-span-5 lg:col-span-5 md:col-span-7 sm:col-span-12 col-span-12">
-                        <div className="right-section">
+                    <div className="xl:col-span-5 lg:col-span-5 md:col-span-7 lg:order-2 order-1 sm:col-span-12 col-span-12">
+                        <div className="right-section sticky top-24">
                             <Shoppingbag />
                         </div>
                     </div>
--- src/app/checkout/shipping-details/index.js
+++ src/app/checkout/shipping-details/index.js
@@ -1,38 +1,44 @@
 'use client'
 import React from "react"
+import { Button } from "@/app/components/ui/button";
 import { useForm } from 'react-hook-form';
 export default function Shippingdetail() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(errors);
-    return (
-        <div className="shipping-details-form">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" placeholder="First name" {...register("First name", { required: true, maxLength: 80 })} />
-                <input type="text" placeholder="Last name" {...register("Last name", { required: true, maxLength: 100 })} />
-                <input type="text" placeholder="Address Line 1" {...register("Address Line 1", { required: true })} />
-                <input type="text" placeholder="Address Line 2" {...register("Address Line 2", { required: true })} />
-                <select {...register("Country", { required: true })}>
-                    <option value="Mr">Mr</option>
-                    <option value="Mrs">Mrs</option>
-                    <option value="Miss">Miss</option>
-                    <option value="Dr">Dr</option>
-                </select>
-                <select {...register("State", { required: true })}>
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
-                </select>
-                <select {...register("City", { required: true })}>
-                    <option value="a">a</option>
-                    <option value="b">b</option>
-                    <option value="c">c</option>
-                </select>
-                <input type="text" placeholder="Pin Code" {...register("Pin Code", { required: true })} />
-                <input type="text" placeholder="Nearest Landmark" {...register("Nearest Landmark", { required: true })} />
-                <input type="text" placeholder="Contact Number" {...register("Contact Number", { required: true })} />
+   const { register, handleSubmit, formState: { errors } } = useForm();
+   const onSubmit = data => console.log(data);
+   console.log(errors);
+   return (
+      <div className="shipping-details-form">
+         <div className="heading text-left border-b pb-3 font-crimson text-[#3c2f27] text-2xl">
+            Shipping and Billing Detail
+         </div>
+         <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="First name" {...register("First name", { required: true, maxLength: 80 })} />
+            <input type="text" placeholder="Last name" {...register("Last name", { required: true, maxLength: 100 })} />
+            <input type="text" placeholder="Address Line 1" {...register("Address Line 1", { required: true })} />
+            <input type="text" placeholder="Address Line 2" {...register("Address Line 2", { required: true })} />
+            <div className='grid grid-cols-3 gap-5'>
+               <select {...register("Country", { required: true })}>
+                  <option value="Mr">Mr</option>
+                  <option value="Mrs">Mrs</option>
+                  <option value="Miss">Miss</option>
+                  <option value="Dr">Dr</option>
+               </select>
+               <select {...register("State", { required: true })}>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+               </select>
+               <select {...register("City", { required: true })}>
+                  <option value="a">a</option>
+                  <option value="b">b</option>
+                  <option value="c">c</option>
+               </select>
+            </div>
+            <input type="text" placeholder="Pin Code" {...register("Pin Code", { required: true })} />
+            <input type="text" placeholder="Nearest Landmark" {...register("Nearest Landmark", { required: true })} />
+            <input type="text" placeholder="Contact Number" {...register("Contact Number", { required: true })} />
 
-                <input type="submit" />
-            </form>
-        </div>
-    )
+            <Button type='submit' className='rounded-none w-full font-roboto h-12 bg-[#b2937e] hover:bg-[#3c2f27]'>Place Order</Button>
+         </form>
+      </div>
+   )
 }

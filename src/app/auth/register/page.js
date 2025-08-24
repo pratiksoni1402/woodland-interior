c0601cd7ad907f35361a8e25425b9dc0ac08a1f1
Pratik <pratiksoni1402@gmail.com>
2025-08-24 09:10:45 +0530

    implementing oauth

--- src/app/auth/register/page.js
+++ src/app/auth/register/page.js
@@ -1,170 +1,170 @@
-"use client";
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
+'use client';
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
 // export const revalidate = 0;
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import axios from "axios";
-import toast, { Toaster } from "react-hot-toast";
-import { UserPlus } from "lucide-react";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
-import { Loader2Icon } from "lucide-react";
-import { Button } from "@/app/components/ui/button";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
+import { UserPlus } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { Loader2Icon } from 'lucide-react';
+import { Button } from '@/app/components/ui/button';
 export default function Register() {
-  const router = useRouter();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const [Error, setError] = useState();
-  const [isLoading, setLoading] = useState(false);
+	const router = useRouter();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+	const [Error, setError] = useState();
+	const [isLoading, setLoading] = useState(false);
 
-  const onSubmit = (data) => {
-    if (data.password === data.cnfpassword) {
-      setLoading(true);
-      axios
-        .post("/api/register-user", data)
-        .then((response) => {
-          if (response.data.successMessage) {
-            router.push("/my-account");
-          } else if (response.data.errorMessage) {
-            toast.error(response.data.errorMessage, {
-              duration: 3000,
-              style: {
-                border: "1px solid #3c2f27",
-                padding: "8px",
-                color: "#faf2ec",
-                backgroundColor: "#3c2f27",
-              },
-              iconTheme: {
-                primary: "#faf2ec",
-                secondary: "#3c2f27",
-              },
-            });
-          }
-        })
-        .catch((error) => {
-          console.error("Registration error", error);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    } else {
-      setError("Password and Confirm Password does not match");
-    }
-  };
-  return (
-    <div className="user-registration-form-wrapper bg-[#faf2ec] pb-12">
-      <div className="container">
-        <div className="grid grid-cols-12">
-          <div className="lg:col-span-3 md:col-span-1 col-span-12"></div>
-          <div className="lg:col-span-6 md:col-span-10 col-span-12">
-            <div className="user-registration">
-              <Toaster />
-              <div className="heading text-center text-2xl text-[#3c2f27] font-crimson py-5">
-                <h1> New Customer ? Signup Here </h1>
-              </div>
-              <div className="register-form">
-                <form
-                  onSubmit={handleSubmit(onSubmit)}
-                  className=" border border-[#b2937e] bg-white p-5"
-                >
-                  <div className="field-wrapper">
-                    <input
-                      type="text"
-                      placeholder="First name"
-                      {...register("firstname", { required: true })}
-                    />
-                    {errors.firstname && (
-                      <span className="error-message font-roboto text-sm text-red-700">
-                        This field is required
-                      </span>
-                    )}
-                  </div>
-                  <div className="field-wrapper">
-                    <input
-                      type="text"
-                      placeholder="Last name"
-                      {...register("lastname", {
-                        required: true,
-                        maxLength: 100,
-                      })}
-                    />
-                    {errors.lastname && (
-                      <span className="error-message font-roboto text-sm text-red-700">
-                        This field is required
-                      </span>
-                    )}
-                  </div>
-                  <div className="field-wrapper">
-                    <input
-                      type="text"
-                      placeholder="Email"
-                      {...register("email", {
-                        required: true,
-                        pattern: /^\S+@\S+$/i,
-                      })}
-                    />
-                    {errors.email && (
-                      <span className="error-message font-roboto text-sm text-red-700">
-                        This field is required
-                      </span>
-                    )}
-                  </div>
-                  <div className="field-wrapper">
-                    <input
-                      type="password"
-                      placeholder="Password"
-                      {...register("password", { required: true })}
-                    />
-                    {errors.password && (
-                      <span className="error-message font-roboto text-sm text-red-700">
-                        This field is required
-                      </span>
-                    )}
-                  </div>
-                  <div className="field-wrapper">
-                    <input
-                      type="password"
-                      placeholder="Confirm Password"
-                      {...register("cnfpassword", { required: true })}
-                    />
-                    {errors.cnfpassword && (
-                      <span className="error-message font-roboto text-sm text-red-700">
-                        This field is required
-                      </span>
-                    )}
-                  </div>
+	const onSubmit = (data) => {
+		if (data.password === data.cnfpassword) {
+			setLoading(true);
+			axios
+				.post('/api/register-user', data)
+				.then((response) => {
+					if (response.data.successMessage) {
+						router.push('/my-account');
+					} else if (response.data.errorMessage) {
+						toast.error(response.data.errorMessage, {
+							duration: 3000,
+							style: {
+								border: '1px solid #3c2f27',
+								padding: '8px',
+								color: '#faf2ec',
+								backgroundColor: '#3c2f27',
+							},
+							iconTheme: {
+								primary: '#faf2ec',
+								secondary: '#3c2f27',
+							},
+						});
+					}
+				})
+				.catch((error) => {
+					console.error('Registration error', error);
+				})
+				.finally(() => {
+					setLoading(false);
+				});
+		} else {
+			setError('Password and Confirm Password does not match');
+		}
+	};
+	return (
+		<div className="user-registration-form-wrapper bg-[#faf2ec] pb-12">
+			<div className="container">
+				<div className="grid grid-cols-12">
+					<div className="lg:col-span-3 md:col-span-1 col-span-12"></div>
+					<div className="lg:col-span-6 md:col-span-10 col-span-12">
+						<div className="user-registration">
+							<Toaster />
+							<div className="heading text-center text-2xl text-[#3c2f27] font-crimson py-5">
+								<h1> New Customer ? Signup Here </h1>
+							</div>
+							<div className="register-form">
+								<form
+									onSubmit={handleSubmit(onSubmit)}
+									className=" border border-[#b2937e] bg-white p-5"
+								>
+									<div className="field-wrapper">
+										<input
+											type="text"
+											placeholder="First name"
+											{...register('firstname', { required: true })}
+										/>
+										{errors.firstname && (
+											<span className="error-message font-roboto text-sm text-red-700">
+												This field is required
+											</span>
+										)}
+									</div>
+									<div className="field-wrapper">
+										<input
+											type="text"
+											placeholder="Last name"
+											{...register('lastname', {
+												required: true,
+												maxLength: 100,
+											})}
+										/>
+										{errors.lastname && (
+											<span className="error-message font-roboto text-sm text-red-700">
+												This field is required
+											</span>
+										)}
+									</div>
+									<div className="field-wrapper">
+										<input
+											type="text"
+											placeholder="Email"
+											{...register('email', {
+												required: true,
+												pattern: /^\S+@\S+$/i,
+											})}
+										/>
+										{errors.email && (
+											<span className="error-message font-roboto text-sm text-red-700">
+												This field is required
+											</span>
+										)}
+									</div>
+									<div className="field-wrapper">
+										<input
+											type="password"
+											placeholder="Password"
+											{...register('password', { required: true })}
+										/>
+										{errors.password && (
+											<span className="error-message font-roboto text-sm text-red-700">
+												This field is required
+											</span>
+										)}
+									</div>
+									<div className="field-wrapper">
+										<input
+											type="password"
+											placeholder="Confirm Password"
+											{...register('cnfpassword', { required: true })}
+										/>
+										{errors.cnfpassword && (
+											<span className="error-message font-roboto text-sm text-red-700">
+												This field is required
+											</span>
+										)}
+									</div>
 
-                  <div className="login pt-5 font-roboto text-sm text-[#3c2f27] hover:underline font-semibold">
-                    <Link href="/auth/login">Back to Log In</Link>
-                  </div>
-                  {isLoading ? (
-                    <Button
-                      type="submit"
-                      className="w-full rounded-none p-3 mt-4 mb-3 border hover:border-[#3c2f27] hover:bg-[#3c2f27] border-[#3c2f27] bg-transparent text-[#3c2f27] hover:text-[#faf2ec] flex justify-center items-center gap-2"
-                      disabled={true}
-                    >
-                      <Loader2Icon className="animate-spin mr-1" />
-                      Signup <UserPlus />
-                    </Button>
-                  ) : (
-                    <Button
-                      type="submit"
-                      className="w-full rounded-none p-3 mt-4 mb-3 border hover:border-[#3c2f27] hover:bg-[#3c2f27] border-[#3c2f27] bg-transparent text-[#3c2f27] hover:text-[#faf2ec] flex justify-center items-center gap-2"
-                    >
-                      Signup <UserPlus />
-                    </Button>
-                  )}
-                </form>
-              </div>
-            </div>
-          </div>
-          <div className="lg:col-span-3 md:col-span-1 col-span-12"></div>
-        </div>
-      </div>
-    </div>
-  );
+									<div className="login pt-5 font-roboto text-sm text-[#3c2f27] hover:underline font-semibold">
+										<Link href="/auth.js/login">Back to Log In</Link>
+									</div>
+									{isLoading ? (
+										<Button
+											type="submit"
+											className="w-full rounded-none p-3 mt-4 mb-3 border hover:border-[#3c2f27] hover:bg-[#3c2f27] border-[#3c2f27] bg-transparent text-[#3c2f27] hover:text-[#faf2ec] flex justify-center items-center gap-2"
+											disabled={true}
+										>
+											<Loader2Icon className="animate-spin mr-1" />
+											Signup <UserPlus />
+										</Button>
+									) : (
+										<Button
+											type="submit"
+											className="w-full rounded-none p-3 mt-4 mb-3 border hover:border-[#3c2f27] hover:bg-[#3c2f27] border-[#3c2f27] bg-transparent text-[#3c2f27] hover:text-[#faf2ec] flex justify-center items-center gap-2"
+										>
+											Signup <UserPlus />
+										</Button>
+									)}
+								</form>
+							</div>
+						</div>
+					</div>
+					<div className="lg:col-span-3 md:col-span-1 col-span-12"></div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/app/components/header/mobile-menu/index.js
+++ src/app/components/header/mobile-menu/index.js
@@ -2,29 +2,26 @@ import React from 'react';
 import Link from 'next/link';
 import { Heart } from 'lucide-react';
 import { ShoppingCart } from 'lucide-react';
-import { Button } from "./../../ui/button";
+import { Button } from './../../ui/button';
 import { CircleUserRound } from 'lucide-react';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "../../ui/dropdown-menu";
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '../../ui/dropdown-menu';
 
 import { Menu } from 'lucide-react';
 import {
-  Sheet,
-  SheetContent,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "../../ui/sheet";
-import {
-  Avatar,
-  AvatarFallback,
-} from "./../../../components/ui/avatar";
+	Sheet,
+	SheetContent,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from '../../ui/sheet';
+import { Avatar, AvatarFallback } from './../../../components/ui/avatar';
 import { BedDouble } from 'lucide-react';
 import { DoorClosed } from 'lucide-react';
 import { Sofa } from 'lucide-react';
@@ -33,164 +30,203 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export default function MobileMenu() {
+	const { data: count } = useQuery({
+		queryKey: ['totalcount'],
+		queryFn: () =>
+			axios
+				.get('/api/cart-items/get-count')
+				.then((response) => {
+					return response.data.productcount;
+				})
+				.catch((error) => {
+					console.log('Error in count', error);
+				}),
+	});
 
-  const { data: count } = useQuery({
-    queryKey: ['totalcount'],
-    queryFn: () =>
-      axios.get('/api/cart-items/get-count')
-        .then((response) => {
-          return response.data.productcount
-        })
-        .catch((error) => {
-          console.log('Error in count', error)
-        })
-  });
+	const { data: wishlisttotal } = useQuery({
+		queryKey: ['wishlistcount'],
+		queryFn: () =>
+			axios
+				.get('/api/wishlist-items/get-count')
+				.then((response) => {
+					return response.data.totalcount;
+				})
+				.catch((error) => {
+					console.log('Error in count', error);
+				}),
+	});
 
-  const { data: wishlisttotal } = useQuery({
-    queryKey: ['wishlistcount'],
-    queryFn: () =>
-      axios.get('/api/wishlist-items/get-count')
-        .then((response) => {
-          return response.data.totalcount
-        })
-        .catch((error) => {
-          console.log('Error in count', error)
-        })
-  });
+	const { data: status } = useQuery({
+		queryKey: ['loginCheck'],
+		queryFn: () =>
+			axios
+				.post('/api/auth-check')
+				.then((response) => {
+					return response.data.userstatus;
+				})
+				.catch((error) => {
+					console.log('Error in Login Status', error);
+				}),
+	});
 
-  const { data:status } = useQuery({
-    queryKey: ['loginCheck'],
-    queryFn: () =>
-      axios.post('/api/auth-check')
-        .then((response) => {
-          return response.data.userstatus
-        })
-        .catch((error) => {
-          console.log('Error in Login Status', error)
-        })
-  });
+	const { data: sessionData } = useQuery({
+		queryKey: ['checkSession'],
+		queryFn: () =>
+			axios
+				.get('/api/get-sessiondata')
+				.then((response) => {
+					return response.data.getSessionData;
+				})
+				.catch((error) => {
+					console.log('Error occured', error);
+				}),
+	});
 
-  const { data: sessionData } = useQuery({
-    queryKey: ['checkSession'],
-    queryFn: () =>
-      axios.get('/api/get-sessiondata')
-        .then((response) => {
-          return response.data.getSessionData
-        })
-        .catch((error) => {
-          console.log("Error occured", error)
-        })
-  })
+	return (
+		<div className="mobile-menu-wrapper">
+			<div className="inner-content-wrapper flex items-center">
+				<div className="inner-wrapper flex">
+					<div className="pr-4">
+						<Link href="/wishlist" className="relative">
+							<Heart />
+							<div className="absolute text-[10px] text-center top-[-10px] right-[-9px] bg-[#3c2f27] text-white rounded-full w-5 h-5 p-[3px]">
+								{wishlisttotal}
+							</div>
+						</Link>
+					</div>
+					<div className="pr-4">
+						<Link href="/cart" className="relative">
+							<ShoppingCart />
+							<div className="absolute text-[10px] text-center top-[-10px] right-[-9px] bg-[#3c2f27] text-white rounded-full w-5 h-5 p-[3px]">
+								{count}
+							</div>
+						</Link>
+					</div>
+					<div className="pr-4">
+						{sessionData && sessionData.user_details ? (
+							<Link href="/my-account">
+								<Avatar className="text-[#3c2f27] border">
+									<AvatarFallback>
+										{sessionData.user_details?.firstname?.charAt(0)}
+										{sessionData.user_details?.lastname?.charAt(0)}
+									</AvatarFallback>
+								</Avatar>
+							</Link>
+						) : (
+							<Link href="/auth.js/login">
+								<CircleUserRound />
+							</Link>
+						)}
+					</div>
+				</div>
+				<div className="side-menu">
+					<Sheet className="">
+						<SheetTrigger asChild>
+							<Button variant="outline triger-button" className="px-0">
+								<Menu />
+							</Button>
+						</SheetTrigger>
+						<SheetContent className="bg-[#faf2ec]">
+							<SheetHeader className="border-b border-[#3c2f27]">
+								<SheetTitle className="font-crimson sm:text-2xl text-lg text-[#3c2f27] pt-4 pb-2">
+									Woodland Interiors
+								</SheetTitle>
+							</SheetHeader>
+							<div className="sheet-body pt-2 font-crimson border-b">
+								<div className="nav-list">
+									<div className="pb-2 px-4 text-lg text-[#3c2f27]">
+										<Link
+											href="/"
+											className="group hover:underline transition duration-500 text-sm font-roboto"
+										>
+											Home
+										</Link>
+									</div>
+									<div className="dropdown">
+										<DropdownMenu className="">
+											<DropdownMenuTrigger asChild>
+												<Button
+													variant="outline"
+													className="pb-1  text-sm font-roboto text-[#3c2f27] bg-transparent border-0 mt-[-13px] hover:bg-transparent group hover:underline font-normal transition duration-500"
+												>
+													Shop
+												</Button>
+											</DropdownMenuTrigger>
+											<DropdownMenuContent className="w-48 bg-[#faf2ec] rounded-none left-[-20px] top-[-10px] fixed">
+												<DropdownMenuGroup>
+													<DropdownMenuItem className=" hover:bg-transparent">
+														<BedDouble
+															className="mr-2 h-4 w-4"
+															color="#3c2f27"
+														/>
+														<Link
+															href="/shop/bedroom"
+															className="group text-[#3c2f27] text-sm font-roboto hover:underline transition duration-500"
+														>
+															Bedroom
+														</Link>
+													</DropdownMenuItem>
+													<DropdownMenuItem className="">
+														<DoorClosed
+															className="mr-2 h-4 w-4"
+															color="#3c2f27"
+														/>
+														<Link
+															href="/shop/dining-tables"
+															className="group text-[#3c2f27] text-sm font-roboto hover:underline transition duration-500"
+														>
+															Dining Tables
+														</Link>
+													</DropdownMenuItem>
+													<DropdownMenuItem>
+														<Sofa className="mr-2 h-4 w-4" color="#3c2f27" />
+														<Link
+															href="/shop/sofa-sets"
+															className="group text-[#3c2f27] text-sm font-roboto hover:underline transition duration-500"
+														>
+															Sofa Sets
+														</Link>
+													</DropdownMenuItem>
+													<DropdownMenuItem>
+														<TentTree
+															className="mr-2 h-4 w-4"
+															color="#3c2f27"
+														/>
+														<Link
+															href="/shop/outdoor"
+															className="group text-[#3c2f27] text-sm font-roboto hover:underline transition duration-500"
+														>
+															Outdoor
+														</Link>
+													</DropdownMenuItem>
+												</DropdownMenuGroup>
+											</DropdownMenuContent>
+										</DropdownMenu>
+									</div>
+									<div className="pb-1 px-4  text-lg text-[#3c2f27]">
+										<Link
+											href="/our-values"
+											className="group hover:underline text-sm font-roboto transition duration-500"
+										>
+											Our Values
+										</Link>
+									</div>
 
-  return (
-    <div className='mobile-menu-wrapper'>
-      <div className='inner-content-wrapper flex items-center'>
-        <div className='inner-wrapper flex'>
-          <div className='pr-4'>
-            <Link href="/wishlist" className='relative'>
-              <Heart />
-              <div className='absolute text-[10px] text-center top-[-10px] right-[-9px] bg-[#3c2f27] text-white rounded-full w-5 h-5 p-[3px]'>{wishlisttotal}</div>
-
-            </Link>
-          </div>
-          <div className='pr-4'>
-            <Link href="/cart" className='relative'>
-              <ShoppingCart />
-              <div className='absolute text-[10px] text-center top-[-10px] right-[-9px] bg-[#3c2f27] text-white rounded-full w-5 h-5 p-[3px]'>{count}</div>
-
-            </Link>
-          </div>
-          <div className='pr-4'>
-            {
-              sessionData && sessionData.user_details ? (
-                <Link href="/my-account">
-                  <Avatar className='text-[#3c2f27] border'>
-                    <AvatarFallback>
-                      {sessionData.user_details?.firstname?.charAt(0)}
-                      {sessionData.user_details?.lastname?.charAt(0)}
-                    </AvatarFallback>
-                  </Avatar>
-                </Link>
-              ) : (
-                <Link href="/auth/login">
-                  <CircleUserRound />
-                </Link>
-              )
-            }
-          </div>
-        </div>
-        <div className='side-menu'>
-          <Sheet className=''>
-            <SheetTrigger asChild>
-              <Button variant="outline triger-button" className='px-0'><Menu /></Button>
-            </SheetTrigger>
-            <SheetContent className='bg-[#faf2ec]'>
-              <SheetHeader className='border-b border-[#3c2f27]'>
-                <SheetTitle className='font-crimson sm:text-2xl text-lg text-[#3c2f27] pt-4 pb-2'>Woodland Interiors</SheetTitle>
-              </SheetHeader>
-              <div className="sheet-body pt-2 font-crimson border-b">
-                <div className='nav-list'>
-                  <div className='pb-2 px-4 text-lg text-[#3c2f27]'>
-                    <Link href='/' className="group hover:underline transition duration-500 text-sm font-roboto">
-                      Home
-                    </Link>
-                  </div>
-                  <div className='dropdown'>
-                    <DropdownMenu className=''>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className='pb-1  text-sm font-roboto text-[#3c2f27] bg-transparent border-0 mt-[-13px] hover:bg-transparent group hover:underline font-normal transition duration-500'>
-                          Shop
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent className="w-48 bg-[#faf2ec] rounded-none left-[-20px] top-[-10px] fixed">
-                        <DropdownMenuGroup>
-                          <DropdownMenuItem className=' hover:bg-transparent'>
-                            <BedDouble className="mr-2 h-4 w-4" color='#3c2f27' />
-                            <Link href='/shop/bedroom' className="group text-[#3c2f27] text-sm font-roboto hover:underline transition duration-500">
-                              Bedroom
-                            </Link>
-                          </DropdownMenuItem>
-                          <DropdownMenuItem className=''>
-                            <DoorClosed className="mr-2 h-4 w-4" color='#3c2f27' />
-                            <Link href='/shop/dining-tables' className="group text-[#3c2f27] text-sm font-roboto hover:underline transition duration-500">
-                              Dining Tables
-                            </Link>
-                          </DropdownMenuItem>
-                          <DropdownMenuItem>
-                            <Sofa className="mr-2 h-4 w-4" color='#3c2f27' />
-                            <Link href='/shop/sofa-sets' className="group text-[#3c2f27] text-sm font-roboto hover:underline transition duration-500">
-                              Sofa Sets
-                            </Link>
-                          </DropdownMenuItem>
-                          <DropdownMenuItem>
-                            <TentTree className="mr-2 h-4 w-4" color='#3c2f27' />
-                            <Link href='/shop/outdoor' className="group text-[#3c2f27] text-sm font-roboto hover:underline transition duration-500">
-                              Outdoor
-                            </Link>
-                          </DropdownMenuItem>
-                        </DropdownMenuGroup>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </div>
-                  <div className='pb-1 px-4  text-lg text-[#3c2f27]'>
-                    <Link href='/our-values' className="group hover:underline text-sm font-roboto transition duration-500">
-                      Our Values
-                    </Link>
-                  </div>
-
-                  <div className='pb-2 px-4  text-lg text-[#3c2f27]'>
-                    <Link href='/contact' className="group hover:underline text-sm font-roboto transition duration-500">
-                      Contact
-                    </Link>
-                  </div>
-                </div>
-              </div>
-              <SheetFooter>
-              </SheetFooter>
-            </SheetContent>
-          </Sheet>
-        </div>
-      </div>
-    </div>
-  )
+									<div className="pb-2 px-4  text-lg text-[#3c2f27]">
+										<Link
+											href="/contact"
+											className="group hover:underline text-sm font-roboto transition duration-500"
+										>
+											Contact
+										</Link>
+									</div>
+								</div>
+							</div>
+							<SheetFooter></SheetFooter>
+						</SheetContent>
+					</Sheet>
+				</div>
+			</div>
+		</div>
+	);
 }

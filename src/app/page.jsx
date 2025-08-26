745932681b443f548353d11758eb1cec62ad5058
pratik <pratiksoni1402@gmail.com>
2025-08-26 10:47:20 +0530

    upgraded tailwind and shadcn

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 			{/* Banner Section */}
 			<section className="banner-wrapper mt-0">
 				<div className="content-wrapper relative">
-					<div className="banner-title text-white z-[1] xl:text-5xl md:text-[55px] text-2xl">
+					<div className="banner-title text-white z-1 xl:text-5xl md:text-[55px] text-2xl">
 						<h1 className="tracking-wide md:leading-[60px] leading-6">
 							Crafted Excellence in Wood:
 						</h1>

f0ede565881a799345f8e53bc969aa7c04fc722c
pratik <pratiksoni1402@gmail.com>
2025-09-09 18:56:10 +0530

    bug fix in schema file

--- src/app/api/product-listing/route.js
+++ src/app/api/product-listing/route.js
@@ -2,6 +2,7 @@ import prisma from '@/db';
 export async function GET(request) {
 	const { searchParams } = new URL(request.url);
 	const slug = searchParams.get('slug');
+	console.log('ss', slug);
 	let productlist = await prisma.category.findMany({
 		where: {
 			slug: slug,

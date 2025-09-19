1bdb1dd51477fecab1fc5f56f1a5fdf36965724f
pratik <pratiksoni1402@gmail.com>
2025-09-19 12:06:40 +0530

    added metadata for plp

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,6 +1,11 @@
 import GetProducts from '@/app/products/components/get-products';
 import { Suspense } from 'react';
 import ProductsFallback from '@/app/products/components/skeletons/fallback';
+
+export const metadata = {
+	title: 'Browse our wide range of products and shop the best deals today.',
+};
+
 export default function ProductListing() {
 	return (
 		<Suspense

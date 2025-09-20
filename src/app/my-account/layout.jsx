075bd5ae2721aaf3fb80846b2e097a7321b225af
Pratik <pratiksoni1402@gmail.com>
2025-09-20 17:41:48 +0530

    adjusted responsiveness issue

--- src/app/my-account/layout.jsx
+++ src/app/my-account/layout.jsx
@@ -16,7 +16,7 @@ export default async function UserProfileLayout({ children }) {
 				<h1 className="capitalize md:text-4xl text-2xl">{greetingMessage}</h1>
 			</div>
 
-			<div className="grid grid-cols-12 gap-10 mb-20">
+			<div className="grid grid-cols-12 sm:gap-10 gap-4 mb-20">
 				<div className="lg:col-span-3 col-span-12 self-start lg:order-1 order-2 !sticky bg-white top-20 bottom-0">
 					<div className="font-crimson text-lg ">
 						<SideMenu />

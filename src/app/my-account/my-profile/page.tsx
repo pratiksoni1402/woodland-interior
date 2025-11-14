8630b49fed9b747398c74c9fd3f31f0317b888ea
pratik <pratiksoni1402@gmail.com>
2025-11-14 18:50:42 +0530

    enabling old component

--- src/app/my-account/my-profile/page.tsx
+++ src/app/my-account/my-profile/page.tsx
@@ -1,5 +1,5 @@
 import UserProfile from '@/app/my-account/components/user-profile';
-import { UserProfileForm } from '@/app/my-account/components/user-profile/components/form';
+// import { UserProfileForm } from '@/app/my-account/components/user-profile/components/form';
 export default function MyProfile() {
-	return <UserProfileForm />;
+	return <UserProfile />;
 }

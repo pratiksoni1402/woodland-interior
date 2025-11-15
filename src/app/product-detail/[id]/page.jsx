5959b6590253b6221fd122842938842a435e5975
Pratik <pratiksoni1402@gmail.com>
2025-11-15 19:53:56 +0530

    profile form created

--- src/app/product-detail/[id]/page.jsx
+++ src/app/product-detail/[id]/page.jsx
@@ -1,7 +1,7 @@
 import ProductDetails from '@/app/product-detail/components/product';
 
 export async function generateMetadata({ params }) {
-	const { id } = params;
+	const { id } = await params;
 	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 	const res = await fetch(`${baseUrl}api/product-detail/${id}`);
